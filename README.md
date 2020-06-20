53aea112d6c80adf9f9414a800911fcd33a97ba7
Paul Sammut <paul@pop-os.localdomain>
2020-06-20 13:48:40 +1000

    Updated README with examples and added .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # msd
+
+## examples
+
+```
+
+arrayOf({ days: 6 }, { weekday: 'long' }).from(today)
+[ 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ]
+
+arrayOf({ days: 6 }, toDate).from(today).map(formatfp({ weekday: 'long' }))
+[ 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ]
+
+arrayOf({ days: 6 }, toDate).from(today)
+[
+  2020-06-19T14:00:00.000Z,
+  2020-06-20T14:00:00.000Z,
+  2020-06-21T14:00:00.000Z,
+  2020-06-22T14:00:00.000Z,
+  2020-06-23T14:00:00.000Z,
+  2020-06-24T14:00:00.000Z
+]
+
+arrayOf({ days: 6 }).from(today).map(toDate)
+[
+  2020-06-19T14:00:00.000Z,
+  2020-06-20T14:00:00.000Z,
+  2020-06-21T14:00:00.000Z,
+  2020-06-22T14:00:00.000Z,
+  2020-06-23T14:00:00.000Z,
+  2020-06-24T14:00:00.000Z
+]
+
+arrayOf({ weeks: 4 }).from(today).map(toDate)
+[
+  2020-06-19T14:00:00.000Z,
+  2020-06-26T14:00:00.000Z,
+  2020-07-03T14:00:00.000Z,
+  2020-07-10T14:00:00.000Z
+]
+
+format(now, {weekday: 'long'})
+Saturday
+
+format(now,  toDate)
+2020-06-20T03:46:18.779Z
+
+arrayOf(days, { weekday: 'long' }).between(today, today + days * 7)
+[
+  'Saturday',
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday'
+]
+
+toDate(today + weeks * 2 + days * 2)
+2020-07-05T14:00:00.000Z
+```
+
+
--- examples.js
+++ examples.js
@@ -3,33 +3,49 @@ const { arrayOf, days, now, hours, weeks, today, format, formatfp, toDate } = re
 // const c0 = arrayOf(days).between(today - days * 2, today - days * 2 + days * 6).map(formatfp({ weekday: 'long' }))
 // console.log(c0)
 
-console.log(days)
+//console.log(days)
 
 const f1 = arrayOf({ days: 6 }, { weekday: 'long' }).from(today)
+console.log()
+console.log("arrayOf({ days: 6 }, { weekday: 'long' }).from(today)")
 console.log(f1)
 
 const f2 = arrayOf({ days: 6 }, toDate).from(today).map(formatfp({ weekday: 'long' }))
+console.log()
+console.log("arrayOf({ days: 6 }, toDate).from(today).map(formatfp({ weekday: 'long' }))")
 console.log(f2)
 
 const f3 = arrayOf({ days: 6 }, toDate).from(today)
+console.log()
+console.log("arrayOf({ days: 6 }, toDate).from(today)")
 console.log(f3)
 
 const f4 = arrayOf({ days: 6 }).from(today).map(toDate)
+console.log()
+console.log("arrayOf({ days: 6 }).from(today).map(toDate)")
 console.log(f4)
 
 const f5 = arrayOf({ weeks: 4 }).from(today).map(toDate)
+console.log()
+console.log("arrayOf({ weeks: 4 }).from(today).map(toDate)")
 console.log(f5)
 
-const f6 = format(now,  {weekday: 'long'})
+const f6 = format(now, {weekday: 'long'})
+console.log()
+console.log("format(now, {weekday: 'long'})")
 console.log(f6)
 
 const f7 = format(now,  toDate)
+console.log()
+console.log("format(now,  toDate)")
 console.log(f7)
 
 const b1 = arrayOf(days, { weekday: 'long' }).between(today, today + days * 7)
+console.log()
+console.log("arrayOf(days, { weekday: 'long' }).between(today, today + days * 7)")
 console.log(b1)
 
-
 const a1 = toDate(today + weeks * 2 + days * 2)
-
+console.log()
+console.log("toDate(today + weeks * 2 + days * 2)")
 console.log(a1)
